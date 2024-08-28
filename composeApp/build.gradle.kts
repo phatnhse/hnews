6afc9c6f29c73858e03dbbd65cb3b700ce00ac24
phatnhse <phatnhse@gmail.com>
2024-08-28 20:39:55 +0200

    Revert "Update versions"
    This reverts commit 90cc13ef5940fa373c15fec59fff6808d1d22948.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -41,6 +41,7 @@ kotlin {
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.runtime.compose)
             implementation(libs.androidx.room.runtime)
             implementation(libs.sqlite.bundled)
             api(libs.koin.core)
