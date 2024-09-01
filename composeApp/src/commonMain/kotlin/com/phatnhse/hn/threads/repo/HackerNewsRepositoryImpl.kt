6890c963d7790f32a7f5dc02378c186419db8964
Phat Nguyen <phatnhse@gmail.com>
2024-09-01 23:05:01 +0200

    Add mappers

--- composeApp/src/commonMain/kotlin/com/phatnhse/hn/threads/repo/HackerNewsRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/phatnhse/hn/threads/repo/HackerNewsRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.phatnhse.hn.threads.repo
 
 import com.phatnhse.hn.news.datasource.HnRemoteDataSource
+import com.phatnhse.hn.news.response.HnStoryResponse
 import com.phatnhse.hn.threads.database.AppDatabase
 import com.phatnhse.hn.threads.database.entity.HackerNewItem
 import com.phatnhse.hn.threads.database.entity.HackerNewsStory
@@ -18,27 +19,11 @@ class HackerNewsRepositoryImpl(
     override suspend fun syncFeed() {
         withContext(Dispatchers.IO) {
             val topStories = hnRemoteDataSource.getTopStories(30)
-            val items = topStories.map { story ->
-                HackerNewItem(
-                    id = story.id,
-                    title = story.title,
-                    url = story.url.orEmpty(),
-                    score = story.score,
-                    by = story.by,
-                    time = story.time,
-                    type = story.type,
-                    descendants = story.descendants ?: 0,
-                    kids = story.kids ?: emptyList(),
-                    text = story.text.orEmpty()
-                )
-            }
-            val stories = topStories.map { story ->
-                HackerNewsStory(
-                    id = 0,
-                    itemId = story.id,
-                    time = story.time
-                )
-            }
+                .filter {
+                    it.dead == false || it.deleted == false
+                }
+            val items = topStories.map { it.toHackerNewItem() }
+            val stories = topStories.map { it.toHackerNewsStory() }
             appDatabase.hackerNewsDao().insertItems(items)
             appDatabase.hackerNewsDao().insertStories(stories)
         }
@@ -49,3 +34,22 @@ class HackerNewsRepositoryImpl(
             .flowOn(Dispatchers.IO)
     }
 }
+
+private fun HnStoryResponse.toHackerNewItem() = HackerNewItem(
+    id = id,
+    title = title,
+    url = url.orEmpty(),
+    score = score,
+    by = by,
+    time = time,
+    type = type,
+    descendants = descendants ?: 0,
+    kids = kids ?: emptyList(),
+    text = text.orEmpty()
+)
+
+private fun HnStoryResponse.toHackerNewsStory() = HackerNewsStory(
+    id = 0,
+    itemId = id,
+    time = time
+)
